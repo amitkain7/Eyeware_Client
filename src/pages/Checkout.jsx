a6ec7f9b4bb42bf3db89b52ca15d4556649b2bdf
amitkain7 <kainturagolu688@gmail.com>
2023-11-06 20:13:00 +0530

    change

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import { useGlobalContext } from '../context/context'
 import { useNavigate } from 'react-router-dom'
-
+import {ToastContainer, toast} from'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Checkout = () => {
   const { storeGlobal, setTotal, resetAmount, removeCart } = useGlobalContext()
   const { total } = storeGlobal
+  
+
   const navigate = useNavigate()
   const initPayment = (data) => {
+
     const options = {
       key: "rzp_test_ClU8FLSvKyioKw",
       amount: data.amount,
@@ -17,52 +21,62 @@ const Checkout = () => {
       description: "Test Transaction",
       order_id: data.id,
       handler: async (response) => {
-        try {
-          const token = localStorage.getItem('token')
+        console.log(response)
+        const token = localStorage.getItem('token')
+        const res = await fetch('https://eye-back.vercel.app/api/payment/verify', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(response)
+        })
+        const data = await res.json()
 
-          const res = await fetch('https://eye-back.vercel.app/api/payment/verify', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(response)
+        if (data.msg === 'payment verified successfully') {
+          setTotal()
+          resetAmount()
+          removeCart()
+          toast.success('🦄 Order Successfull !', {
+            theme : 'dark'
           })
-          const data = await res.json()
-          if(data.msg == 'payment verified successfully'){
-
-            setTotal()
-            resetAmount()
-            removeCart()
+          setTimeout(() => {
             navigate('/')
-          }
-        } catch (error) {
-          console.log(error);
+          }, 2000)
+
         }
+
+
+      },
+      prefill: {
+        name: "eyeSome",
+        email: "eyesome@gamil.com",
+        contact: "9999999999"
       },
       theme: {
-        color: "#3399cc",
-      },
-    };
+        "color": "#121212"
+      }
+    }
 
     const razor = new window.Razorpay(options);
     razor.open();
   };
 
+
   const handlePayment = async () => {
     try {
       const token = localStorage.getItem('token')
-
       const res = await fetch('https://eye-back.vercel.app/api/payment/order', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
+          'authorization': `Bearer ${token}`
+
         },
         body: JSON.stringify({ amount: total })
       })
       const data = await res.json()
-     
+
       initPayment(data.data);
     } catch (error) {
       console.log(error);
@@ -71,17 +85,24 @@ const Checkout = () => {
   return (
     <>
       <Navbar />
+      
 
-      {total > 0 && <div className='flex flex-col justify-between items-center  h-40 shadow m-auto rounded-lg mt-20 w-60 '>
-        <h1 className='text-xl bg-blue-800 w-full text-white text-center '>Payment page</h1>
-        <h1>Total :  <span>₹ {total}</span></h1>
-        <button onClick={handlePayment} className='mb-6 bg-blue-600 text-white py-1 px-2 rounded-lg hover:bg-blue-800' >
-          confirm payment
-        </button>
-      </div>
-      }
+        {total > 0 && <div className='flex flex-col justify-between items-center  h-40 shadow m-auto rounded-lg mt-20 w-60 '>
+          <h1 className='text-xl bg-blue-800 w-full text-white text-center '>Payment page</h1>
+          <h1>Total :  <span>₹ {total}</span></h1>
+          <button onClick={handlePayment} className='mb-6 bg-blue-600 text-white py-1 px-2 rounded-lg hover:bg-blue-800' >
+            confirm payment
+          </button>
+        </div>
+
+        }
+        <ToastContainer/>
+     
+
     </>
   )
 }
 
 export default Checkout
+
+
